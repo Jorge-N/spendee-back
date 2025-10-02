443c2e27066c1ac4ef5a6a7dcb0793fef33c2abd
Jorge_N <valentinnave@gmail.com>
2025-10-02 17:02:32 -0300

    add gasto por id

--- Spendee/index.js
+++ Spendee/index.js
@@ -45,6 +45,35 @@ app.get("/gasto", validateToken, async (req, res) => {
   res.json(gastos)
 })
 
+app.get("/gasto/:userId", validateToken, async (req, res) => {
+  const { userId } = req.params
+  try {
+    const userExpenses = await prisma.gasto.findMany({
+      where: { usuarioId: userId },
+    })
+    res.status(200).json(userExpenses)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+app.get("/gastoPorId/:id", validateToken, async (req, res) => {
+  const id = parseInt(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "ID inválido" })
+  }
+
+  try {
+    const expense = await prisma.gasto.findUnique({
+      where: { id },
+    })
+    if (!expense) return res.status(404).json({ error: "Gasto no encontrado" })
+    res.status(200).json(expense)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
 app.post("/ingreso", validateToken, async (req, res) => {
   const { userId, ingreso, montoAnterior } = req.body
   try {
