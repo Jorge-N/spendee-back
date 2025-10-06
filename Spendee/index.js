3bd9a2e660d5a1e12284a65e85358f1e79048cfa
Jorge_N <valentinnave@gmail.com>
2025-10-06 20:19:59 -0300

    add income by id and user incomes routes

--- Spendee/index.js
+++ Spendee/index.js
@@ -98,6 +98,35 @@ app.get("/ingreso", validateToken, async (req, res) => {
   res.json(ingresos)
 })
 
+app.get("/ingresoPorId/:id", validateToken, async (req, res) => {
+  const id = parseInt(req.params.id)
+  if (isNaN(id)) {
+    return res.status(400).json({ error: "ID inválido" })
+  }
+
+  try {
+    const income = await prisma.ingreso.findUnique({
+      where: { id },
+    })
+    if (!income) return res.status(404).json({ error: "Gasto no encontrado" })
+    res.status(200).json(income)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
+app.get("/ingreso/:userId", validateToken, async (req, res) => {
+  const { userId } = req.params
+  try {
+    const userIncomes = await prisma.ingreso.findMany({
+      where: { usuarioId: userId },
+    })
+    res.status(200).json(userIncomes)
+  } catch (error) {
+    res.status(400).json({ error: error.message })
+  }
+})
+
 app.get("/balance/:userId", validateToken, async (req, res) => {
   const { userId } = req.params
   try {
