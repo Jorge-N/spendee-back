5c88391cfef407500be8e09c4f54c36d88fb6676
TriniSuarezC <trinisuarezcarreras@gmail.com>
2025-10-10 23:15:26 -0300

    fix

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
 const serverless = require("serverless-http")
-const app = require("../index.js")
-console.log("Loaded app for serverless")
+const express = require("express")
+const { PrismaClient } = require("@prisma/client")
+const { PrismaNeon } = require("@prisma/adapter-neon")
+
+const app = express()
+let prisma
+if (!global.__prisma) {
+  const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL })
+  global.__prisma = new PrismaClient({ adapter })
+}
+prisma = global.__prisma
+
+app.use(express.json())
+
+app.get("/health", (req, res) => {
+  res.status(200).send("OK")
+})
+
 module.exports = serverless(app)
