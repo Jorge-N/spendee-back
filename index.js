dcc4a59b83eed0ebbedfd151214fffb6a10e6ef8
TriniSuarezC <trinisuarezcarreras@gmail.com>
2025-10-11 23:24:33 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const serverless = require("serverless-http")
 const express = require("express")
+const { PrismaClient } = require("@prisma/client")
+
+const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
