46a35e108c66a15cda98973e9c79593ee699923d
Juan Lavagnini <91229986+juanlavagnini@users.noreply.github.com>
2025-11-12 16:37:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1000,9 +1000,9 @@ app.get("/hasAPISecret", validateToken, async (req, res) => {
 
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`)
 })
 
-module.exports = app
+module.exports = serverless(app)
